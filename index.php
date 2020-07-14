d6fbb29b1569812ef7842c36091665f7261dd562
Giosue1994 <giosuelentini1994@outlook.com>
2020-07-14 16:51:30 +0200

    php snack 3

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
-<!-- Creiamo un array 'matches' contenente altri array i quali
+<?php
+/* Snack 1:
+Creiamo un array 'matches' contenente altri array i quali
 rappresentano delle partite di basket di un’ipotetica tappa del
 calendario. Ogni array della partita avrà una squadra di casa e una
 squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla
 squadra ospite.
 Stampiamo a schermo tutte le partite con questo schema:
- Olimpia Milano - Cantù | 55-60 -->
+ Olimpia Milano - Cantù | 55-60 */
 
-<?php
  $matches = [
   [
    'squadra_1' => 'Milano',
@@ -28,6 +29,19 @@ Stampiamo a schermo tutte le partite con questo schema:
   ]
  ];
 
+ /* Snack 2:
+Passare come parametri GET name, mail e age e verificare
+(cercando i metodi che non conosciamo nella documentazione) che:
+1. name sia più lungo di 3 caratteri,
+2. che mail contenga un punto e una chiocciola
+3. e che age sia un numero.
+Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+ */
+
+ $nome = $_GET['name'];
+ $email = $_GET['mail'];
+ $eta = $_GET['age'];
+
 ?>
 
 <!DOCTYPE html>
@@ -44,11 +58,20 @@ Stampiamo a schermo tutte le partite con questo schema:
 
         <li>
           <?php echo $this_partita['squadra_1'] ?> - <?php echo $this_partita['squadra_2'] ?>
-          <?php echo $this_partita['punti_1'] ?> - <?php echo $this_partita['punti_2'] ?>
+          <?php echo $this_partita['punti_1'] ?> | <?php echo $this_partita['punti_2'] ?>
         </li>
 
       <?php } ?>
     </ul>
+
+    <p>
+      <?php if ((strlen($nome) > 3) && (strpos($email, '.') !== false) && (strpos($email, '@') !== false) && (is_numeric($eta))) { ?>
+        <?php echo 'Accesso riuscito' ?>
+      <?php }
+      else { ?>
+        <?php echo 'Accesso negato' ?>
+      <?php } ?>
+    </p>
 
   </body>
 </html>
